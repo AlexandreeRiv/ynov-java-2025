d1d79d8f9019568d002f963278d51c00e5f6938b
Alexandre <alexandreriviere111@gmail.com>
2025-03-04 16:22:27 +0100

    exercice(Loops):add exercice 'While'

---
+++ src/fr/ynov/java/easy/While.java
@@ -0,0 +1,12 @@
+package fr.ynov.java.easy;
+
+public class While {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 10;
+        while ( i <= j){
+            i++;
+            System.out.println(i);
+        }
+    }
+}
