a556d755129738d4d5f3782b1a733229f3184a03
Alexandre <alexandreriviere111@gmail.com>
2025-03-04 16:25:06 +0100

    exercice(Loops):add exercice 'DoWhile'

---
+++ src/fr/ynov/java/easy/DoWhile.java
@@ -0,0 +1,12 @@
+package fr.ynov.java.easy;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 10;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= j);
+    }
+}
