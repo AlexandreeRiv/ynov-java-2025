c2b1240be31df953365ca28b3dd84c34ef2b97f1
Alexandre <alexandreriviere111@gmail.com>
2025-03-11 10:49:31 +0100

    exercices(Classes and Enum):Final exercices with the calcutore of age

--- src/fr/ynov/java/medium/Person.java
+++ src/fr/ynov/java/medium/Person.java
@@ -1,30 +1,49 @@
 package fr.ynov.java.medium;
 
-enum Nationality {
-    France, Germany , American , Canadian, Russian
-}
+import javax.management.ValueExp;
+import java.time.LocalDate;
+import java.time.Period;
+
 
 public class Person {
+    private enum Nationality {
+        France, Germany , American , Canadian, Russian
+    }
     //attributes of person
     private String name;
-    private int age;
+    private String birthDate;
     private String gender;
     private int height;
     private int weight;
     Nationality nationality;
 
 //Constructor
-    Person(String name, int age, String gender, int height, int weight , Nationality nationality) {
+    Person(String name, String birthDate,String gender, int height, int weight , Nationality nationality) {
         this.name = name;
-        this.age = age;
+        this.birthDate = birthDate;
         this.gender = gender;
         this.height = height;
         this.weight = weight;
         this.nationality = nationality;
     }
 
+    public String getBirthDate() {
+        return birthDate;
+    }
 
     public static void main(String[] args) {
 
+
+        // create a Person
+        Person moi = new Person( "Alexandre" , "2004-06-02" , "male" , 170 , 90 , Nationality.France );
+        //transform the string by a Localdate
+        LocalDate newBirthDate = LocalDate.parse(moi.getBirthDate());
+        // take the dates of the day put it into date
+        LocalDate date = LocalDate.now();
+        //put the period between those two dates
+        Period period = Period.between(newBirthDate, date);
+
+
+        System.out.println("vous avez " + period.getYears() + " ans");
     }
 }
